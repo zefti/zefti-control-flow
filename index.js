37fc899b1b62351d75a77b1f487bf7f72e17f78d
Ryan Ogle <ryan@zefti.com>
2015-07-19 13:44:37 -0700

    stuff

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function waterfall(){
   var req = args[0];
   var res = args[1];
   var cb = function(){
-    if (arguments[0]) return errorHandler(arguments[0], res);
+    if (arguments[0]) return errorHandler.send(arguments[0], res);
     position++;
     var args = Array.prototype.slice.call(arguments);
     args.shift();
     args.push(cb);
     functionList[position].apply(functionList[position], args);
-  }
+  };
   args.pop();
   args.pop();
   args.push(cb);
